b764888da6a0b531577ae7c5b0155f0aa12db2f2
citizenzet <exgamer@live.ru>
2019-11-01 16:04:13 +0600

    +

--- src/actions/web/CreateLocalizedAction.php
+++ src/actions/web/CreateLocalizedAction.php
@@ -1,7 +1,7 @@
 <?php
 namespace concepture\yii2logic\actions\web;
 
-use concepture\yii2logic\actions\Action;
+use concepture\yii2logic\actions\web\CreateAction;
 use Yii;
 use yii\db\Exception;
 use concepture\yii2logic\actions\traits\LocalizedTrait;
@@ -14,29 +14,9 @@ use concepture\yii2logic\actions\traits\LocalizedTrait;
  * @package concepture\yii2logic\actions\web
  * @author Olzhas Kulzhambekov <exgamer@live.ru>
  */
-class CreateLocalizedAction extends Action
+class CreateLocalizedAction extends CreateAction
 {
     use LocalizedTrait;
-
-//    public $view = 'create';
-//    public $redirect = 'view';
-//    public $serviceMethod = 'create';
-
-//    public function run($locale = "ru")
-//    {
-//        $model = $this->getForm();
-//        $model->locale = $locale;
-//        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-//            if (($result = $this->getService()->{$this->serviceMethod}($model)) != false) {
-//                return $this->redirect([$this->redirect, 'id' => $result->id]);
-//            }
-//        }
-//
-//        return $this->render($this->view, [
-//            'model' => $model,
-//            'params' => Yii::$app->request->getQueryParams(),
-//        ]);
-//    }
 
     protected function processModel($model)
     {
