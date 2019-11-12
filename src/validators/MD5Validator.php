f6c6a2959206342dbb0ef7612b43429c301c26b1
citizenzet <exgamer@live.ru>
2019-11-12 14:41:37 +0600

    +

---
+++ src/validators/MD5Validator.php
@@ -0,0 +1,47 @@
+<?php
+namespace concepture\yii2logic\validators;
+
+use Yii;
+use yii\base\Exception;
+use yii\validators\Validator;
+use yii\db\ActiveRecord;
+
+/**
+ * Валидатор переводит в MD5 выбранный атрибут, используя значение $toAttr
+ *
+ * @author Kamaelkz
+ */
+class MD5Validator extends Validator
+{
+    /**
+     * Атрибут источник для MD5
+     * @var string
+     */
+    public $source;
+
+    /**
+     * Менять значение при изменениии
+     * @var boolean
+     */
+    public $changeOnEdit = true;
+    public $skipOnEmpty = false;
+    public function init()
+    {
+        parent::init();
+        if (! $this->source) {
+            throw new Exception(Yii::t('yii', 'Свойство {$source} должно быть установлено.'));
+        }
+    }
+
+    public function validateAttribute($model, $attribute)
+    {
+        if($model->{$attribute} && ! $this->changeOnEdit){
+            return;
+        }
+        $result = $model->{$this->source};
+        if(is_array($result)){
+            $result = reset($result);
+        }
+        $model->{$attribute} = md5($result);
+    }
+}
