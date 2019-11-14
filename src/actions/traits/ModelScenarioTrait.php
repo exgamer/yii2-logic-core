951dccccaff3c3090e09ed528c7e87ed2aec39ff
citizenzet <exgamer@live.ru>
2019-11-14 11:46:31 +0600

    +

--- src/actions/traits/ModelScenarioTrait.php
+++ src/actions/traits/ModelScenarioTrait.php
@@ -22,8 +22,49 @@ trait ModelScenarioTrait
                 ScenarioEnum::UPDATE => $this->attributes()
             ]
         );
+        $extendedScenarios = $this->extendedScenarios();
+        foreach ($extendedScenarios as $name => $excludedAttributes){
+            $this->addCustomScenario($scenarios, $name, $excludedAttributes);
+        }
 
         return $scenarios;
+    }
+
+    /**
+     * Добавить сценарий
+     *
+     * @param array $scenarios
+     * @param string $name
+     * @param array $excludedAttributes
+     */
+    protected function addCustomScenario(array &$scenarios, string $name, array $excludedAttributes)
+    {
+        $scenarios = parent::scenarios();
+        $attributes = $this->attributes();
+        $attributes = array_flip($attributes);
+        foreach ($excludedAttributes as $attribute){
+            unset($attributes[$attribute]);
+        }
+        $attributes = array_flip($attributes);
+        $scenarios[$name] = $attributes;
+    }
+
+    /**
+     * Возвращает массив с кастомными сценариями
+     * где ключ название сценария а значение массив с аттрибутами которые нужно исключить
+     *
+     * [
+     *      'сценарий1' => [
+     *          'ненужный атрибут1',
+     *          'ненужный атрибут2'
+     *      ]
+     * ]
+     *
+     * @return array
+     */
+    protected function extendedScenarios()
+    {
+        return [];
     }
 }
 
