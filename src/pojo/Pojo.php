c4f63685021a53cccfb082f3ad555ff2790f8a1f
i.skiba <ilya@media.bet>
2019-11-18 15:20:38 +0600

    fix validators, add base pojo

---
+++ src/pojo/Pojo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace concepture\yii2logic\pojo;
+
+use concepture\yii2logic\forms\Model;
+use concepture\yii2logic\helpers\StringHelper;
+
+/**
+ * Базовый класс для проверки данных
+ *
+ * @author kamaelkz <kamaelkz@yandex.kz>
+ */
+abstract class Pojo extends Model
+{
+    /**
+     * @inheritDoc
+     */
+    public function load($data, $formName = null)
+    {
+        if(
+            is_string($data)
+            && StringHelper::isJson($data)
+        ) {
+            $data = StringHelper::jsonToArray($data);
+        }
+
+        return parent::load($data, $formName);
+    }
+}
