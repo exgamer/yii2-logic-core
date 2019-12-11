3f403c77a60fa91551abe60369943e4b5dbb441c
citizenzet <exgamer@live.ru>
2019-12-11 16:29:52 +0600

    +

--- src/services/traits/CatalogTrait.php
+++ src/services/traits/CatalogTrait.php
@@ -1,250 +1,111 @@
 <?php
-namespace concepture\yii2logic\services\traits;
+namespace concepture\yii2handbook\services;
 
-use concepture\yii2logic\enum\IsDeletedEnum;
-use concepture\yii2logic\enum\StatusEnum;
+use concepture\yii2handbook\models\Domain;
+use concepture\yii2logic\services\Service;
+use yii\helpers\Url;
 use Yii;
-use Exception;
-use yii\helpers\ArrayHelper;
-use yii\db\ActiveQuery;
-use yii\db\ActiveRecord;
 
 /**
- * Треит сервиса содержащий методы для реализации методов, которые помогают использовать сущность как справочник
- * Получать записи как массив ключ => значение
- * и ключи и значения из массива
- *
- * Trait CatalogTrait
- * @package concepture\yii2logic\services\traits
+ * Class DomainService
+ * @package concepture\yii2handbook\service
  * @author Olzhas Kulzhambekov <exgamer@live.ru>
  */
-trait CatalogTrait
+class DomainService extends Service
 {
     /**
-     * Возвращает массив с каталогом записей
-     * Для использования у search модели должны быть определены методы
-     * getListSearchAttribute и getListSearchKeyAttribute
+     * Возвращает id текущего домена
      *
-     *   таким способом можно вызывать методы модели
-     *   например если в модели сделать метод  getLabel()
-     *   если метод getListSearchAttribute вернет 'label'
-     *   будет вызван метод getLabel() модели
+     * @param bool $reset
      *
-     *
-     * @return array
-     * @throws Exception
+     * @return int|null
      */
-    public function catalog()
+    public function getCurrentDomainId($reset = false)
     {
-        static $_catalog = null;
-        if (! empty($_catalog)){
-            return $_catalog;
+        static $result;
+
+        if($result && ! $reset) {
+
+            return $result;
         }
 
-        $searchClass = $this->getRelatedSearchModelClass();
-        $searchKey = $searchClass::getListSearchKeyAttribute();
-        $searchAttr = $searchClass::getListSearchAttribute();
-        if (! $searchKey || ! $searchAttr){
-            throw new Exception("please realize getListSearchKeyAttribute() and getListSearchAttribute() in ".$searchClass);
+        if (! isset(Yii::$app->params['yii2handbook'])){
+
+            return null;
         }
 
-        $_catalog = $this->getAllList($searchKey, $searchAttr);
+        if (! isset(Yii::$app->params['yii2handbook']['domainMap'])){
 
-        return $_catalog;
-    }
-
-    /**
-     * Возвращает воличество элементов каталога
-     *
-     * @return int
-     * @throws Exception
-     */
-    public function catalogCount()
-    {
-        $catalog = $this->catalog();
-
-        return count($catalog);
-    }
-
-    /**
-     * Доп действия перед получением ключа по значению
-     * @param $value
-     * @param $catalog
-     * @return mixed
-     */
-    protected function catalogKeyPreAction(&$value, &$catalog){}
-
-    /**
-     * Возвращает ключ из каталога по значению
-     * Для использования у search модели должны быть определены методы
-     * getListSearchAttribute и getListSearchKeyAttribute
-     *
-     * @param $value
-     * @return mixed|null
-     * @throws Exception
-     */
-    public function catalogKey($value)
-    {
-        $catalog = $this->catalog();
-        $catalog = array_flip($catalog);
-        $this->catalogKeyPreAction($value, $catalog);
-        if (isset($catalog[$value])){
-            return $catalog[$value];
+            return null;
         }
 
-        return null;
-    }
-
-    /**
-     * Возвращает значение из каталога по ключу
-     * Для использования у search модели должны быть определены методы
-     * getListSearchAttribute и getListSearchKeyAttribute
-     *
-     * @param $key
-     * @return mixed|null
-     * @throws Exception
-     */
-    public function catalogValue($key)
-    {
-        $catalog = $this->catalog();
-        if (isset($catalog[$key])){
-            return $catalog[$key];
+        $domainMap = Yii::$app->params['yii2handbook']['domainMap'];
+        $host = $this->getCurrentHost();
+        if(! $host) {
+            return null;
         }
 
-        return null;
-    }
-
-    /**
-     * Возвращает данные для исползования с виджетом \yii\jui\AutoComplete
-     *
-     *   таким способом можно вызывать методы модели
-     *   например если в модели сделать метод  getLabel()
-     *   если метод getListSearchAttribute вернет 'label'
-     *   будет вызван метод getLabel() модели
-     *
-     * @param null $term
-     * @return array []
-     * @throws Exception
-     */
-    public function getAutocompleteList($term = null)
-    {
-        if(!$term){
-            return [];
-        }
-
-        $searchClass = $this->getRelatedSearchModelClass();
-        $tableName = $searchClass::tableName();
-        $searchKey = $searchClass::getListSearchKeyAttribute();
-        $searchAttr = $searchClass::getListSearchAttribute();
-        if (! $searchKey || ! $searchAttr){
-            throw new Exception("please realize getListSearchKeyAttribute() and getListSearchAttribute() in ".$searchClass);
-        }
-        $where = [];
-//        $model = new $searchClass();
-//        if ($model->hasAttribute('status')){
-//            $where['status'] = StatusEnum::ACTIVE;
-//        }
-//        if ($model->hasAttribute('is_deleted')){
-//            $where['is_deleted'] = IsDeletedEnum::NOT_DELETED;
-//        }
-
-        $query = $this->getQuery()
-            ->select(["{$tableName}.{$searchAttr} as value", "{$tableName}.{$searchAttr} as  label","{$tableName}.{$searchKey} as id"])
-            ->andWhere(['like', $searchAttr, $term])
-            ->andWhere($where)
-            ->asArray();
-        $this->extendCatalogTraitQuery($query);
-
-        return $query->all();
-    }
-
-    /**
-     * Возвращает массив даных для выпадающих списков
-     * Для использования у search модели должны быть определены методы
-     * getListSearchAttribute и getListSearchKeyAttribute
-     *
-     *   таким способом можно вызывать методы модели
-     *   например если в модели сделать метод  getLabel()
-     *   если метод getListSearchAttribute вернет 'label'
-     *   будет вызван метод getLabel() модели
-     *
-     *
-     * @param array $queryParams
-     * @param string $formName
-     * @return array
-     * @throws Exception
-     */
-    public function getDropDownList($queryParams = [], $formName = "")
-    {
-        $searchClass = $this->getRelatedSearchModelClass();
-        $searchModel = Yii::createObject($searchClass);
-        $searchAttribute = $searchClass::getListSearchAttribute();
-        if (! $searchAttribute){
-            throw new Exception("please realize  getListSearchAttribute() in ".$searchClass);
-        }
-        if ($searchModel->hasAttribute('status')) {
-            $queryParams['status'] = StatusEnum::ACTIVE;
-        }
-        if ($searchModel->hasAttribute('is_deleted')) {
-            $queryParams['is_deleted'] = IsDeletedEnum::NOT_DELETED;
-        }
-        $dataProvider = $this->getDataProvider($queryParams, [], null, $formName);
-
-        return ArrayHelper::map($dataProvider->getModels(), $searchClass::getListSearchKeyAttribute(), $searchAttribute);
-    }
-
-
-    /**
-     * Для расширения запроса для вывода каталога и списка для выпадашек
-     *
-     * @param ActiveQuery $query
-     */
-    protected function extendCatalogTraitQuery(ActiveQuery $query){}
-
-    /**
-     * Возвращает массив моделей для выпадающих списков
-     *
-     * @param array $where
-     * @param bool $excludeDefault
-     * @return mixed
-     */
-    public function getAllModelsForList($where = [], $excludeDefault = false)
-    {
-        if ($excludeDefault === false) {
-            $modelClass = $this->getRelatedModelClass();
-            $model = Yii::createObject($modelClass);
-            if ($model->hasAttribute('status')) {
-                $where['status'] = StatusEnum::ACTIVE;
+        if (! isset($domainMap[$host])){
+            $domainMap = array_flip($domainMap);
+            if (! isset($domainMap[$host])){
+                return null;
             }
-            if ($model->hasAttribute('is_deleted')) {
-                $where['is_deleted'] = IsDeletedEnum::NOT_DELETED;
-            }
+
+            $host = $domainMap[$host];
+            $domainMap = array_flip($domainMap);
         }
 
-        $query = $this->getQuery()->andWhere($where);
-        $this->extendCatalogTraitQuery($query);
+        $domainAlias = $domainMap[$host];
+        $domains = $this->catalog(false);
+        $domains = array_flip($domains);
+        if (! isset($domains[$domainAlias])){
 
-        return $query->all();
+            return null;
+        }
+
+        return $domains[$domainAlias];
     }
 
     /**
-     * Возвращает массив записей таблицы для выпадающих списков
+     * Возвращает текущий хост
      *
-     *   таким способом можно вызывать методы модели
-     *   например если в модели сделать метод  getLabel()
-     *   если метод getListSearchAttribute вернет 'label'
-     *   будет вызван метод getLabel() модели
-     *
-     * @param string $from
-     * @param $to
-     * @param array $where
-     * @param boolean $excludeDefault
-     * @return mixed
+     * @return string
      */
-    public function getAllList($from = 'id', $to, $where = [], $excludeDefault = false)
+    public function getCurrentHost()
     {
-        $models = $this->getAllModelsForList($where, $excludeDefault);
+        static $result;
 
-        return ArrayHelper::map($models, $from , $to);
+        if($result) {
+            return $result;
+        }
+
+        $currentDomain = null;
+        if (Yii::$app instanceof \yii\web\Application) {
+            $currentDomain = Url::base(true);
+        }
+        $parsed = parse_url($currentDomain);
+
+        return $parsed['host'];
+    }
+
+    /**
+     * Возвращает запись текущего домена
+     *
+     * @param bool $reset
+     *
+     * @return Domain
+     */
+    public function getCurrentDomain($reset = false)
+    {
+        static $result;
+
+        if($result && ! $reset) {
+            return $result;
+        }
+
+        $currentDomainId = $this->getCurrentDomainId();
+        $result = $this->getCatalogModel($currentDomainId);
+
+        return $result;
     }
 }
