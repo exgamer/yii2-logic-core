d73e02483296b15349fb7d3a93bd34046fcb7313
Olzhas Kulzhambekov <exgamer@live.ru>
2019-12-16 16:02:30 +0600

    1

---
+++ src/enum/EditorTypeEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace concepture\yii2logic\enum;
+
+/**
+ * Class EditorTypeEnum
+ * @package concepture\yii2logic\enum
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class EditorTypeEnum extends Enum
+{
+    const CKE = 1;
+    const FROALA = 2;
+}
