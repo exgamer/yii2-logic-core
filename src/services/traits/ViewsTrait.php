d414f28bccce6fabc595939e4e0cb759d9d6c033
Olzhas Kulzhambekov <exgamer@live.ru>
2020-01-02 20:55:46 +0600

    1

--- src/services/traits/ViewsTrait.php
+++ src/services/traits/ViewsTrait.php
@@ -19,26 +19,16 @@ trait ViewsTrait
     public function processViewCount($model)
     {
         $table = $this->getTableName();
-        $key = $table . '_views';
-        $session = Yii::$app->session;
-        // Если в сессии отсутствуют данные,
-        // создаём, увеличиваем счетчик, и записываем в бд
-        if (!isset($session[$key])) {
-            $session->set($key, [$model->id]);
+        $sessionId = Yii::$app->session->getId();
+        $key = $sessionId . "_" . $table . '_view_' . $model->id;
+        $cache = Yii::$app->cache;
+        $data = $cache->get($key);
+        if ($data === false) {
+            $cache->set($key, [$model->id], 20);
             $model->views++;
             $model->save(false);
-        // Если в сессии уже есть данные то проверяем засчитывался ли данный пост
-        // если нет то увеличиваем счетчик, записываем в бд и сохраняем в сессию просмотр этого поста
-        } else {
-            if (!ArrayHelper::isIn($model->id, $session[$key])) {
-                $array = $session[$key];
-                array_push($array, $model->id);
-                $session->remove($key);
-                $session->set($key, $array);
-                $model->views++;
-                $model->save(false);
-            }
         }
+
         return true;
     }
 }
