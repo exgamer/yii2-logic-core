fa5500969ffd08f1e9d4ec6061ad5b1ed907e457
Olzhas Kulzhambekov <exgamer@live.ru>
2020-01-10 16:41:10 +0600

    +

--- src/services/traits/CopyTrait.php
+++ src/services/traits/CopyTrait.php
@@ -46,5 +46,39 @@ trait CopyTrait
      * @param ActiveRecord $model
      */
     protected function afterCopy(ActiveRecord $model){}
+
+    /**
+     * Создание копии сущности для кросс доменных сущностей
+     *
+     * @param Model $form
+     * @param ActiveRecord $model
+     * @return ActiveRecord
+     */
+    public function copyBetweenDomains(Model $form, ActiveRecord $model)
+    {
+        $form->domain_id = Yii::$app->domainService->getCurrentDomainId();
+        $this->beforeCopyBetweenDomains($form, $model);
+        $model = $this->update($form, $model);
+        if (! $model){
+            return false;
+        }
+
+        $this->afterCopyBetweenDomains($model);
+
+        return $model;
+    }
+
+    /**
+     * Дополнительные действия с моделью перед созданием копии
+     * @param Model $form класс для работы
+     * @param ActiveRecord $model
+     */
+    protected function beforeCopyBetweenDomains(Model $form, ActiveRecord $model){}
+
+    /**
+     * Дополнительные действия с моделью после создания копии
+     * @param ActiveRecord $model
+     */
+    protected function afterCopyBetweenDomains(ActiveRecord $model){}
 }
 
