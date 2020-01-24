6c55877b67c0926cdcbc44e38ad52cce174942c6
Olzhas Kulzhambekov <exgamer@live.ru>
2020-01-24 16:23:01 +0600

    +

--- src/services/Service.php
+++ src/services/Service.php
@@ -59,7 +59,7 @@ abstract class Service extends Component
     {
         $modelClass = $this->getRelatedModelClass();
 
-        return $modelClass::tableName();
+        return trim($modelClass::tableName(), '{}');
     }
 
     /**
