25bf1e85ead196bcf94d0fcfd207f6951fd6330a
Olzhas Kulzhambekov <exgamer@live.ru>
2020-01-27 14:19:37 +0600

    +

---
+++ src/enum/ServiceEventEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace concepture\yii2logic\enum;
+
+/**
+ * Class ServiceEventEnum
+ * @package concepture\yii2logic\enum
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class ServiceEventEnum extends Enum
+{
+    const EVENT_AFTER_BATCH_INSERT = 'afterBatchInsert';
+}
