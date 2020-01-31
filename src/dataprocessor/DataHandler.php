9d9a5d81ac0b66dc79b1d34d06516e611b98e02b
Olzhas Kulzhambekov <exgamer@live.ru>
2020-01-31 15:52:28 +0600

    +

--- src/dataprocessor/DataHandler.php
+++ src/dataprocessor/DataHandler.php
@@ -17,7 +17,7 @@ abstract class DataHandler implements DataHandlerInterface
      * @param type $inputData
      * @return bool
      */
-    public static function isExecute($processor, &$inputData = null)
+    public static function isExecute(DataProcessor $processor, &$inputData = null)
     {
         return true;
     }
@@ -26,36 +26,36 @@ abstract class DataHandler implements DataHandlerInterface
      * Действия до запуска query
      * @param array $inputData
      */
-    public static function beforeExecute($processor, &$inputData = null){}
+    public static function beforeExecute(DataProcessor $processor, &$inputData = null){}
     /**
      * дествия после завершения всех запросов
      * @param array $inputData
      */
-    public static function afterExecute($processor, &$inputData = null){}
+    public static function afterExecute(DataProcessor $processor, &$inputData = null){}
     /**
      * Действия после завершения операции
      * @param type $data
      * @param type $inputData
      */
-    public static function finishProcess($processor, &$data, &$inputData = null){}
+    public static function finishProcess(DataProcessor $processor, &$data, &$inputData = null){}
 
     /**
      * Действия после завершения обработки 1 страницы данных
      * @param type $inputData
      */
-    public static function afterPageProcess($processor, &$inputData = null){}
+    public static function afterPageProcess(DataProcessor $processor, &$inputData = null){}
     /**
      * returns array of prepared data
      * вносим необходимые изменения в данные
      *
      * return array
      */
-    public static function prepareData($processor, &$data){}
+    public static function prepareData(DataProcessor $processor, &$data){}
 
     /**
      * Выполняем необходимые действия с данными
      */
-    public static function processData($processor, &$data, &$inputData = null){}
+    public static function processData(DataProcessor $processor, &$data, &$inputData = null){}
 
     /**
      * настройка основного запроса
@@ -67,6 +67,6 @@ abstract class DataHandler implements DataHandlerInterface
     /**
      * Показываем сообщение после выполнения
      */
-    public static function showMessage($processor, $isUpdate, $model, $endMessage){}
+    public static function showMessage(DataProcessor $processor, $isUpdate, $model, $endMessage){}
 }
 
