cd221401edc7fd1285d0b6192324890128d7ddde
Olzhas Kulzhambekov <exgamer@live.ru>
2020-02-04 14:39:58 +0600

    +

--- src/services/traits/StatusTrait.php
+++ src/services/traits/StatusTrait.php
@@ -17,7 +17,9 @@ trait StatusTrait
         $this->beforeStatusChange($model, $status);
         $model->status = $status;
         $model->save(false);
+        $this->afterStatusChange($model, $status);
     }
 
     protected function beforeStatusChange(ActiveRecord $model, $status){}
+    protected function afterStatusChange(ActiveRecord $model, $status){}
 }
