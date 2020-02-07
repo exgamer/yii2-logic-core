dcf3d6cbb7f08c5010c6cadef93883ae1bc68e45
Olzhas Kulzhambekov <exgamer@live.ru>
2020-02-07 21:03:42 +0600

    1

--- src/helpers/UrlHelper.php
+++ src/helpers/UrlHelper.php
@@ -36,7 +36,7 @@ class UrlHelper
      * @param string $moduleId
      * @return string
      */
-    protected static function getLocation($model, $urlParamAttrs, $actionId = 'view', &$controllerId = null, $moduleId = null)
+    public static function getLocation($model, $urlParamAttrs, $actionId = 'view', &$controllerId = null, $moduleId = null)
     {
         $queryParams = [];
         foreach ($urlParamAttrs as $attribute){
