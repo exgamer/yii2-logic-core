0025af0a0ca8e323468cf33ea0bddaa31b1202a3
Olzhas Kulzhambekov <exgamer@live.ru>
2020-02-07 21:02:30 +0600

    1

--- src/helpers/UrlHelper.php
+++ src/helpers/UrlHelper.php
@@ -3,6 +3,8 @@
 namespace concepture\yii2logic\helpers;
 
 use Yii;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Inflector;
 
 /**
  * Class UrlHelper
@@ -24,6 +26,37 @@ class UrlHelper
         $urlManagerConfig['baseUrl'] = "";
 
         return Yii::createObject($urlManagerConfig);
+    }
+
+    /**
+     * @param $model
+     * @param $urlParamAttrs
+     * @param string $actionId
+     * @param string $controllerId
+     * @param string $moduleId
+     * @return string
+     */
+    protected static function getLocation($model, $urlParamAttrs, $actionId = 'view', &$controllerId = null, $moduleId = null)
+    {
+        $queryParams = [];
+        foreach ($urlParamAttrs as $attribute){
+            $queryParams[$attribute] = $model->{$attribute};
+        }
+
+        $className = ClassHelper::getShortClassName($model);
+        if (! $controllerId) {
+            $controllerId = Inflector::camel2id($className);
+        }
+
+        $url = $controllerId . "/" . $actionId;
+        if ($moduleId){
+            $url = $moduleId . "/" . $url;
+        }
+
+        $urlParams = ArrayHelper::merge([$url], $queryParams);
+        $frontendUrlManager = static::getFrontendUrlManager();
+
+        return $frontendUrlManager->createUrl($urlParams);
     }
 }
 
