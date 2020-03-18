b20a0d56150017a5a08d4271b96fdfb0d6b4a1a6
Olzhas Kulzhambekov <exgamer@live.ru>
2020-03-18 11:38:59 +0600

    обновлен after batch insert

---
+++ src/enum/SocialEnum.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace concepture\yii2logic\enum;
+
+use concepture\yii2logic\enum\Enum;
+
+/**
+ * Список соцсетей
+ * @package common\enum
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class SocialEnum extends Enum
+{
+    const FACEBOOK = 'facebook';
+    const TWITTER = 'twitter';
+    const INSTAGRAM = 'instagram';
+    const GOOGLE = 'google';
+    const VKONTAKTE = 'vkontakte';
+    const MAILRU = 'mailru';
+    const ODNOCLASSNIKI = 'odnoklassniki';
+    const YANDEX = 'yandex';
+    const YOUTUBE = 'youtube';
+
+    public static function labels()
+    {
+        return [
+            self::FACEBOOK => Yii::t('common', "Facebook"),
+            self::TWITTER => Yii::t('common', "Twitter"),
+            self::INSTAGRAM => Yii::t('common', "Instagram"),
+            self::GOOGLE => Yii::t('common', "Google"),
+            self::VKONTAKTE => Yii::t('common', "Vkontakte"),
+            self::MAILRU => Yii::t('common', "Mailru"),
+            self::ODNOCLASSNIKI => Yii::t('common', "Odnoklassniki"),
+            self::YANDEX => Yii::t('common', "Yandex"),
+            self::YOUTUBE => Yii::t('common', "Youtube")
+        ];
+    }
+}
