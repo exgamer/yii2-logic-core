0606a1cd6f1be65ece381e055092bfecb8020a33
Olzhas Kulzhambekov <exgamer@live.ru>
2020-03-20 17:06:44 +0600

    поведение для json

--- src/enum/StatusEnum.php
+++ src/enum/StatusEnum.php
@@ -19,8 +19,8 @@ class StatusEnum extends Enum
     public static function labels()
     {
         return [
-            self::ACTIVE => Yii::t('core', "Активный"),
-            self::INACTIVE => Yii::t('core', "Неактивный"),
+            self::ACTIVE => Yii::t('core', "Опубликован"),
+            self::INACTIVE => Yii::t('core', "Черновик"),
         ];
     }
 }
