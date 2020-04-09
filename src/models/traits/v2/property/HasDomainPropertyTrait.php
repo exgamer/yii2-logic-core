633269f80e1edbeb485ebac6b6c6d215d83b044d
Olzhas Kulzhambekov <exgamer@live.ru>
2020-04-09 13:47:47 +0600

    дополнение треита

--- src/models/traits/v2/property/HasDomainPropertyTrait.php
+++ src/models/traits/v2/property/HasDomainPropertyTrait.php
@@ -1,6 +1,7 @@
 <?php
 namespace concepture\yii2logic\models\traits\v2\property;
 
+use concepture\yii2logic\models\traits\HasLocalizationTrait;
 use Yii;
 
 /**
@@ -29,5 +30,39 @@ trait HasDomainPropertyTrait
     public static function uniqueFieldValue()
     {
         return Yii::$app->domainService->getCurrentDomainId();
+    }
+
+    /**
+     * Переопределено для автоподстановки domain_id в релейшн
+     * @inheritDoc
+     */
+    protected function createRelationQuery($class, $link, $multiple)
+    {
+        $query = parent::createRelationQuery($class, $link, $multiple);
+        $relationModel = Yii::createObject($class);
+        $domainAlias = "";
+        $traits = ClassHelper::getTraits($relationModel);
+        if (in_array(HasDomainPropertyTrait::class, $traits) ||
+            in_array(HasLocalePropertyTrait::class, $traits)){
+            $propModelClass = $relationModel::getPropertyModelClass();
+            $propModel = Yii::createObject($propModelClass);
+            if ($propModel->hasAttribute('domain_id')){
+                $domainAlias = $relationModel::propertyAlias() . ".";
+            }
+        }
+        if (in_array(HasLocalizationTrait::class, $traits)){
+            $propModelClass = $relationModel::getLocalizationModelClass();
+            $propModel = Yii::createObject($propModelClass);
+            if ($propModel->hasAttribute('domain_id')){
+                $domainAlias = $relationModel::propertyAlias() . ".";
+            }
+        }
+        if ($relationModel->hasAttribute('domain_id')) {
+            $where[$domainAlias . 'domain_id'] = $this->domain_id;
+        }
+        if (!empty($where)) {
+            $query->andWhere($where);
+        }
+        return $query;
     }
 }
