f9170e58953c9a47890e4a03c73a6c21164cb42c
Olzhas Kulzhambekov <exgamer@live.ru>
2020-07-14 12:13:35 +0600

    AR

---
+++ src/pojo/String.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace concepture\yii2logic\pojo;
+
+/**
+ * Модель для данных строкой
+ *
+ * Class Social
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class String extends Pojo
+{
+    public $content;
+
+    /**
+     * @inheritDoc
+     */
+    public function rules()
+    {
+        return [
+            [
+                [
+                    'content',
+                ],
+                'required'
+            ],
+            [
+                [
+                    'content',
+                ],
+                'string'
+            ],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'content' => \Yii::t('core','Текст'),
+        ];
+    }
+}
