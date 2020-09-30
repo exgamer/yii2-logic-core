2936d7f99a04780030c5ae0c2ac2b46a0eee735b
Olzhas Kulzhambekov <exgamer@live.ru>
2020-09-30 12:36:48 +0600

    +

--- src/models/traits/v2/property/HasDomainPropertyTrait.php
+++ src/models/traits/v2/property/HasDomainPropertyTrait.php
@@ -1,6 +1,7 @@
 <?php
 namespace concepture\yii2logic\models\traits\v2\property;
 
+use concepture\yii2logic\db\HasPropertyActiveQuery;
 use concepture\yii2logic\helpers\ClassHelper;
 use concepture\yii2logic\models\traits\HasLocalizationTrait;
 use Yii;
@@ -40,33 +41,52 @@ trait HasDomainPropertyTrait
     protected function createRelationQuery($class, $link, $multiple)
     {
         $query = parent::createRelationQuery($class, $link, $multiple);
+        /**
+         * @TODO Оставил для того чтобы не сломалось чего
+         * это для случаев локализованных сущностей и просто таблиц где есть domain_id
+         */
+        if (! $query instanceof HasPropertyActiveQuery) {
+            $relationModel = Yii::createObject($class);
+            $domainAlias = "";
+            $traits = ClassHelper::getTraits($relationModel);
+            if (in_array(HasDomainPropertyTrait::class, $traits) ||
+                in_array(HasLocalePropertyTrait::class, $traits)){
+                $propModelClass = $relationModel::getPropertyModelClass();
+                $propModel = Yii::createObject($propModelClass);
+                if ($propModel->hasAttribute('domain_id')){
+                    $domainAlias = $relationModel::propertyAlias() . ".";
+                }
+            }
+
+            if (in_array(HasLocalizationTrait::class, $traits)){
+                $propModelClass = $relationModel::getLocalizationModelClass();
+                $propModel = Yii::createObject($propModelClass);
+                if ($propModel->hasAttribute('domain_id')){
+                    $domainAlias = $relationModel::propertyAlias() . ".";
+                }
+            }
+
+            if ($relationModel->hasAttribute('domain_id') && $this->domain_id) {
+                $where[$domainAlias . 'domain_id'] = $this->domain_id;
+            }
+
+            if (!empty($where)) {
+                $query->andWhere($where);
+            }
+
+            return $query;
+        }
+
         $relationModel = Yii::createObject($class);
-        $domainAlias = "";
-        $traits = ClassHelper::getTraits($relationModel);
-        if (in_array(HasDomainPropertyTrait::class, $traits) ||
-            in_array(HasLocalePropertyTrait::class, $traits)){
-            $propModelClass = $relationModel::getPropertyModelClass();
-            $propModel = Yii::createObject($propModelClass);
-            if ($propModel->hasAttribute('domain_id')){
-                $domainAlias = $relationModel::propertyAlias() . ".";
+        $fields = static::getUniqueFieldAsArray();
+        $params = [];
+        foreach ($fields as $key => $attribute) {
+            if ($relationModel->hasAttribute($attribute)) {
+                $params[$attribute] = $this->{$attribute};
             }
         }
 
-        if (in_array(HasLocalizationTrait::class, $traits)){
-            $propModelClass = $relationModel::getLocalizationModelClass();
-            $propModel = Yii::createObject($propModelClass);
-            if ($propModel->hasAttribute('domain_id')){
-                $domainAlias = $relationModel::propertyAlias() . ".";
-            }
-        }
-
-        if ($relationModel->hasAttribute('domain_id') && $this->domain_id) {
-            $where[$domainAlias . 'domain_id'] = $this->domain_id;
-        }
-
-        if (!empty($where)) {
-            $query->andWhere($where);
-        }
+        $query->applyPropertyUniqueValue($params);
 
         return $query;
     }
