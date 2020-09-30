b7eaedc65dc03b49ff520efb5649b5260091607a
Olzhas Kulzhambekov <exgamer@live.ru>
2020-09-30 12:37:29 +0600

    +

--- src/models/traits/v2/property/HasDomainPropertyTrait.php
+++ src/models/traits/v2/property/HasDomainPropertyTrait.php
@@ -77,6 +77,7 @@ trait HasDomainPropertyTrait
             return $query;
         }
 
+        // для случаенв когда связь с доменной сущностью
         $relationModel = Yii::createObject($class);
         $fields = static::getUniqueFieldAsArray();
         $params = [];
