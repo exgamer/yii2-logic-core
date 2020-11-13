97b0a78bcd4e96973de12d35af32163bbd29902c
Olzhas Kulzhambekov <exgamer@live.ru>
2020-11-13 11:47:31 +0600

    ++

--- src/models/DomainBasedDictionaryActiveRecord.php
+++ src/models/DomainBasedDictionaryActiveRecord.php
@@ -5,6 +5,8 @@ use concepture\yii2logic\models\interfaces\HasPropertyInterface;
 use concepture\yii2logic\models\interfaces\IAmDictionaryInterface;
 
 /**
+ * ActiveRecord для моделей которые являются справочниками учитывающими домен
+ *
  * Class DomainBasedDictionaryActiveRecord
  * @package concepture\yii2logic\models
  * @author Olzhas Kulzhambekov <exgamer@live.ru>
