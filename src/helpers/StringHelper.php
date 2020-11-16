616e676dfdeb87b588de34aa933b55b49ba26356
Olzhas Kulzhambekov <exgamer@live.ru>
2020-11-16 12:20:26 +0600

    ++

--- src/helpers/StringHelper.php
+++ src/helpers/StringHelper.php
@@ -31,7 +31,18 @@ class StringHelper extends BaseHelper
      * @param string $country_iso
      * @return bool
      */
-    public static function integerFormat($number, $decimals = 0, $country_iso = 'default')
+    public static function integerFormat($number, $country_iso = 'default')
+    {
+        return static::numberFormat($number, 0, $country_iso);
+    }
+
+    /**
+     * @param $number
+     * @param int $decimals
+     * @param string $country_iso
+     * @return string
+     */
+    public static function numberFormat($number, $decimals = 0, $country_iso = 'default')
     {
         if (isset(static::$number_formats[$country_iso])){
             return number_format($number, $decimals, static::$number_formats[$country_iso][0], static::$number_formats[$country_iso][1]);
