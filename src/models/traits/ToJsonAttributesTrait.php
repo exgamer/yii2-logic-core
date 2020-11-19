ba548e5f01a65b52fea952493a2ad0543a866d01
Eugeny <evgstn7@gmail.com>
2020-11-19 12:30:38 +0600

    json field sort

--- src/models/traits/ToJsonAttributesTrait.php
+++ src/models/traits/ToJsonAttributesTrait.php
@@ -2,6 +2,8 @@
 namespace concepture\yii2logic\models\traits;
 
 use concepture\yii2logic\helpers\StringHelper;
+use yii\data\ActiveDataProvider;
+use yii\db\Expression;
 use yii\helpers\Json;
 
 /**
@@ -134,6 +136,28 @@ trait ToJsonAttributesTrait
         }
 
         return parent::update($runValidation, $attributeNames);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function extendDataProvider(ActiveDataProvider $dataProvider)
+    {
+        $jsonField = $this->jsonFieldName();
+        $dbType = $this->getAttrDbType($jsonField);
+        $sort = $dataProvider->getSort();
+        $jsonAttributes = $this->toJsonAttributes();
+        if (in_array($dbType, ['json', 'jsonb']) && $sort && $jsonAttributes) {
+            foreach ($jsonAttributes as $jsonAttr) {
+                if (isset($sort->attributes[$jsonAttr]['asc'])) {
+                    $sort->attributes[$jsonAttr]['asc'] = new Expression("({$jsonField}->>'$.{$jsonAttr}') ASC");
+                }
+                if (isset($sort->attributes[$jsonAttr]['desc'])) {
+                    $sort->attributes[$jsonAttr]['desc'] = new Expression("({$jsonField}->>'$.{$jsonAttr}') DESC");
+                }
+            }
+        }
+        parent::extendDataProvider($dataProvider);
     }
 }
 
