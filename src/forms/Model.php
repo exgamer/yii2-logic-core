b42fa13226d4405680310d18afdc97fb8dd19c1f
Olzhas Kulzhambekov <exgamer@live.ru>
2020-11-25 11:30:31 +0600

    ++

--- src/forms/Model.php
+++ src/forms/Model.php
@@ -14,4 +14,44 @@ abstract class Model extends Base
 {
     use ModelScenarioTrait;
     use ModelSupportTrait;
+
+    private $old_scenario;
+
+    /**
+     * @return mixed
+     */
+    public function getOldScenario()
+    {
+        return $this->old_scenario;
+    }
+
+    /**
+     * @param mixed $old_scenario
+     */
+    public function setOldScenario($old_scenario)
+    {
+        $this->old_scenario = $old_scenario;
+    }
+
+    /**
+     * Sets the scenario for the model.
+     * Note that this method does not check if the scenario exists or not.
+     * The method [[validate()]] will perform this check.
+     * @param string $value the scenario that this model is in.
+     */
+    public function setScenario($value)
+    {
+        $this->setOldScenario($this->getScenario());
+        parent::setScenario($value);
+    }
+
+    /**
+     *
+     */
+    public function rollbackOldScenario()
+    {
+        if ($this->getOldScenario()) {
+            $this->setScenario($this->getOldScenario());
+        }
+    }
 }
